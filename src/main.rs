158d7b17385fc114c5c41a94ba4b3150e33bdb2b
Davey Moores <daveymoores@gmail.com>
2021-10-12 15:02:44 +0200

    tests for subcommands with options

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate clap;
 use chrono::prelude::*;
 use clap::{App, Arg, ArgMatches, Error};
-use regex::Regex;
 use std::ffi::OsString;
 use std::process;
 
@@ -9,26 +8,7 @@ use std::process;
 struct Cli<'a> {
     matches: ArgMatches<'a>,
     command: Option<String>,
-    options: Option<String>,
-}
-
-fn validate_date_options(v: String) -> Result<(), String> {
-    // create regex for the date string
-    let re = Regex::new(
-        r"(?P<year>\d{4})?\s?(?P<month>\d{1,2})?\s?(?P<day>\d{1,2})?\s?(?P<hour>\d{1,2})",
-    )
-    .unwrap();
-    let date_values = re.captures(&*v).unwrap();
-    let date_value_ok: bool = !&date_values["year"].is_empty()
-        | !&date_values["month"].is_empty()
-        | !&date_values["day"].is_empty()
-        | !&date_values["hour"].is_empty();
-
-    if date_value_ok {
-        Ok(())
-    } else {
-        Err(String::from("The value did not contain numeric values"))
-    }
+    options: Vec<Option<String>>,
 }
 
 impl Cli<'_> {
@@ -41,74 +21,146 @@ impl Cli<'_> {
         I: Iterator<Item = T>,
         T: Into<OsString> + Clone,
     {
+        let hour_arg = Arg::with_name("hour")
+            .short("h")
+            .long("hour")
+            .value_name("xx")
+            .help(
+                "sets the hour value. When the day/month/year \n\
+                    isn't set, it defaults to the current day",
+            )
+            .required(true);
+
+        let day_arg = Arg::with_name("day")
+            .requires("hour")
+            .short("d")
+            .long("day")
+            .value_name("xx")
+            .help(
+                "sets the day value. When the month/year \n\
+                    isn't set, it defaults to the current day",
+            );
+
+        let month_arg = Arg::with_name("month")
+            .requires("day")
+            .short("m")
+            .long("month")
+            .value_name("xx")
+            .help(
+                "sets the day value. When the month/year \n\
+                    isn't set, it defaults to the current day",
+            );
+
+        let year_arg = Arg::with_name("year")
+            .requires("month")
+            .short("y")
+            .long("year")
+            .value_name("xxxx")
+            .help(
+                "sets the day value. When the month/year \n\
+                    isn't set, it defaults to the current day",
+            );
+
         let app: App = App::new("timesheet-gen")
             .version("0.1")
             .author("Davey Moores")
             .about(
                 "Minimal configuration, simple timesheets for sharing via pdf download or unique link.",
-            ).arg(
-            Arg::with_name("init")
-                .short("i")
-                .long("init")
-                .value_name("path")
-                .help(
-                    "Pass optional 'path' to git repository. Defaults \n\
-            to current directory",
-                )
-                .takes_value(true)
-                .min_values(0)
-                .max_values(1),
-        ).arg(
-            Arg::with_name("run-mode")
-                .short("o")
-                .long("run-mode")
-                .value_names(&["cd | d"])
+            ).subcommand(
+            App::new("init")
+                .about("Initialise for current or specified repository")
+                .arg(Arg::with_name("path")
+                    .value_name("path")
+                    .help(
+                        "Pass optional 'path' to git repository. Defaults \n\
+                            to current directory",
+                    )
+                    .required(false)))
+            .subcommand(
+            App::new("run-mode")
+                .about("Whether the repository should \n\
+               be initialised as commit based (cb) or default (d)").arg(
+                Arg::with_name("mode")
+                .value_name("mode")
                 .help(
                     "Whether the repository should \n\
                be initialised as commit based (cb) or default (d)",
                 )
                 .takes_value(true)
                 .min_values(0)
-                .max_values(1),
-        ).arg(
-            Arg::with_name("edit")
-                .short("e")
-                .long("edit")
-                .value_names(&["year", "month", "day", "hour"])
-                .validator(validate_date_options)
-                .help(
-                    "Sets the hours worked for a given day. \n\
-            The arguments are parsed from left to right, and omitted \n\
-            values default to the current day/month/year",
-                )
-                .takes_value(true)
-        ).arg(
-            Arg::with_name("remove")
-                .short("r")
-                .long("remove")
-                .value_names(&["year", "month", "day", "hour"])
-                .help("Removes the work entry for a given day/month/year.")
-                .takes_value(true)
-                .min_values(1)
-                .max_values(4),
-        ).arg(
-            Arg::with_name("make")
-                .short("m")
-                .long("make")
-                .value_names(&["month"])
-                .help(
-                    "Generates a timesheet for a given month. \n\
-            Defaults to last completed calendar month.",
-                )
-                .takes_value(true)
-                .min_values(0)
-                .max_values(1),
-        );
+                .max_values(1)))
+            .subcommand(App::new("edit")
+                .about("Change the hours worked value for a given day")
+                .arg(&hour_arg)
+                .arg(&day_arg)
+                .arg(&month_arg)
+                .arg(&year_arg))
+            .subcommand(App::new("remove")
+                .about("Remove the entry for a given day")
+                .arg(Arg::with_name("day")
+                    .short("d")
+                    .long("day")
+                    .value_name("xx")
+                    .help(
+                        "sets the day value. When the month/year \n\
+                    isn't set, it defaults to the current day",
+                    ))
+                .arg(&month_arg)
+                .arg(&year_arg))
+            .subcommand(App::new("make")
+                .about("Change the hours worked value for a given day")
+                .arg(Arg::with_name("month")
+                    .short("m")
+                    .long("month")
+                    .value_name("xx")
+                    .help(
+                        "sets the day value. When the month/year \n\
+                    isn't set, it defaults to the current day",
+                    ))
+                .arg(&year_arg));
 
         // extract the matches
         let matches = app.get_matches_from_safe(args)?;
 
-        if matches.args.is_empty() {
+        Ok(Cli {
+            matches,
+            command: None,
+            options: vec![None],
+        })
+    }
+
+    pub fn parse_commands(&self, matches: &ArgMatches) -> Result<Cli, clap::Error> {
+        let mut options: Vec<Option<String>> = vec![];
+        let command: Option<String>;
+
+        let date_time: DateTime<Local> = Local::now();
+        let year = date_time.year().to_string();
+        let month = date_time.month().to_string();
+        let day = date_time.day().to_string();
+
+        if let Some(init) = matches.subcommand_matches("init") {
+            // set default value of current directory
+            options.push(Some(init.value_of("path").unwrap_or(".").to_string()));
+            command = Some("init".to_string());
+        } else if let Some(make) = matches.subcommand_matches("make") {
+            // set default value of current month
+            options.push(Some(make.value_of("month").unwrap_or(&*month).to_string()));
+            command = Some("make".to_string());
+        } else if let Some(edit) = matches.subcommand_matches("edit") {
+            // this will error out if the preceding date value isn't passed
+            // so I can happily set default here knowing that just the day/month/year will make it through
+            options.push(Some(edit.value_of("hour").unwrap().to_string()));
+            options.push(Some(edit.value_of("day").unwrap_or(&day).to_string()));
+            options.push(Some(edit.value_of("month").unwrap_or(&month).to_string()));
+            options.push(Some(edit.value_of("year").unwrap_or(&year).to_string()));
+            command = Some("edit".to_string());
+        } else if let Some(remove) = matches.subcommand_matches("remove") {
+            options.push(Some(remove.value_of("day").unwrap_or(&*day).to_string()));
+            command = Some("remove".to_string());
+        } else if let Some(run_mode) = matches.subcommand_matches("run-mode") {
+            options.push(Some(run_mode.value_of("mode").unwrap_or("d").to_string()));
+            command = Some("run_mode".to_string());
+        } else {
             return Err(Error {
                 message: "No matches for inputs".to_string(),
                 kind: clap::ErrorKind::EmptyValue,
@@ -116,56 +168,8 @@ impl Cli<'_> {
             });
         }
 
-        Ok(Cli {
-            matches,
-            command: None,
-            options: None,
-        })
-    }
-
-    pub fn parse_commands(&self, matches: &ArgMatches) -> Result<Cli, clap::Error> {
-        let mut options = None;
-        let mut command = None;
-
-        let date_time: DateTime<Local> = Local::now();
-        let month = date_time.month().to_string();
-        let day = date_time.day().to_string();
-
-        if matches.is_present("init") {
-            options = Some(matches.value_of("init").unwrap_or(".").to_string());
-            command = Some("init".to_string());
-        }
-
-        if matches.is_present("make") {
-            options = Some(matches.value_of("make").unwrap_or(&*month).to_string());
-            command = Some("make".to_string());
-        }
-
-        if matches.is_present("edit") {
-            match matches.value_of("edit") {
-                None => {
-                    return Err(Error {
-                        message: "Missing required argument".to_string(),
-                        kind: clap::ErrorKind::MissingRequiredArgument,
-                        info: None,
-                    });
-                }
-                Some(matches) => {
-                    options = Some(matches.to_string());
-                    command = Some("edit".to_string());
-                }
-            }
-        }
-
-        if matches.is_present("remove") {
-            options = Some(matches.value_of("remove").unwrap_or(&*day).to_string());
-            command = Some("remove".to_string());
-        }
-
-        if matches.is_present("run_mode") {
-            options = Some(matches.value_of("run_mode").unwrap_or("d").to_string());
-            command = Some("run_mode".to_string());
-        }
+        // println!("matches: {:#?}", self.matches);
+        println!("options: {:?}", options);
 
         Ok(Cli {
             options,
@@ -193,34 +197,51 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fmt::Debug;
+    use std::str::FromStr;
+
+    fn unwrap_iter_with_option<T: FromStr + Clone>(args: Vec<Option<String>>) -> Vec<T>
+    where
+        <T as FromStr>::Err: Debug,
+    {
+        args.into_iter()
+            .map(|x| x.unwrap().parse::<T>().unwrap().clone())
+            .collect()
+    }
 
     #[test]
     fn returns_an_error_when_no_command_args_are_passed() {
-        let result = Cli::new_from([""].iter());
+        let cli = Cli::new_from([""].iter()).unwrap();
+        let result = cli.parse_commands(&cli.matches);
         assert!(result.is_err());
         assert_eq!(result.unwrap_err().kind, clap::ErrorKind::EmptyValue);
     }
 
     #[test]
-    fn finds_a_match_from_command_args() {
-        let result = Cli::new_from(["exename", "--init"].iter()).unwrap();
-        assert!(result.matches.args.contains_key("init"));
+    fn finds_a_match_without_options_from_command_args() {
+        let cli = Cli::new_from(["exename", "init"].iter()).unwrap();
+        let m = cli.matches.subcommand_matches("init").unwrap();
+        let (command, _value) = m.subcommand();
+        assert_eq!(command, "");
     }
 
     #[test]
     fn returns_a_default_option_for_init() {
-        let cli: Cli = Cli::new_from(["exename", "--init"].iter()).unwrap();
+        let cli: Cli = Cli::new_from(["exename", "init"].iter()).unwrap();
         let new_cli = cli.parse_commands(&cli.matches);
         let result = new_cli.unwrap();
-        assert_eq!(result.options.unwrap(), ".");
+        let values = unwrap_iter_with_option::<String>(result.options);
+        assert_eq!(values, vec!["."]);
         assert_eq!(result.command.unwrap().clone(), "init");
     }
 
     #[test]
     fn returns_a_passed_value_for_init() {
-        let cli: Cli = Cli::new_from(["exename", "--init", "/usr/path"].iter()).unwrap();
+        let cli: Cli = Cli::new_from(["exename", "init", "/usr/path"].iter()).unwrap();
         let new_cli = cli.parse_commands(&cli.matches);
-        assert_eq!(new_cli.unwrap().options.unwrap(), "/usr/path");
+        let result = new_cli.unwrap();
+        let values = unwrap_iter_with_option::<String>(result.options);
+        assert_eq!(values, vec!["/usr/path"]);
     }
 
     #[test]
@@ -228,41 +249,54 @@ mod tests {
         let date_time: DateTime<Local> = Local::now();
         let month = date_time.month();
 
-        let cli: Cli = Cli::new_from(["exename", "--make"].iter()).unwrap();
+        let cli: Cli = Cli::new_from(["exename", "make"].iter()).unwrap();
         let new_cli = cli.parse_commands(&cli.matches);
         let result = new_cli.unwrap();
-        assert_eq!(
-            result.options.unwrap().parse::<u32>().unwrap().clone(),
-            month
-        );
+        let values = unwrap_iter_with_option::<u32>(result.options);
+        assert_eq!(values, vec![month]);
         assert_eq!(result.command.unwrap().clone(), "make");
     }
 
     #[test]
     fn returns_a_passed_value_for_make() {
-        let cli: Cli = Cli::new_from(["exename", "--make", "10"].iter()).unwrap();
+        let cli: Cli = Cli::new_from(["exename", "make", "-m5"].iter()).unwrap();
         let new_cli = cli.parse_commands(&cli.matches);
-
-        assert_eq!(
-            new_cli.unwrap().options.unwrap().parse::<u32>().unwrap(),
-            10
-        );
+        let result = new_cli.unwrap();
+        let values = unwrap_iter_with_option::<u32>(result.options);
+        assert_eq!(values, vec![5]);
     }
 
     #[test]
     fn returns_an_error_when_no_arg_is_present_for_edit() {
-        let result = Cli::new_from(["exename", "--edit"].iter());
+        let result = Cli::new_from(["exename", "edit"].iter());
         assert!(result.is_err());
     }
 
-    // #[test]
-    // fn returns_a_passed_value_for_edit() {
-    //     let cli: Cli = Cli::new_from(["exename", "--edit", "2021 10 30 6"].iter()).unwrap();
-    //     let new_cli = cli.parse_commands(&cli.matches);
-    //
-    //     assert_eq!(
-    //         new_cli.unwrap().options.unwrap().parse::<u32>().unwrap(),
-    //         10
-    //     );
-    // }
+    #[test]
+    fn returns_an_error_when_a_day_is_passed_without_an_hour() {
+        let result = Cli::new_from(["exename", "edit", "-d8"].iter());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn returns_an_error_when_a_month_is_passed_without_a_day() {
+        let result = Cli::new_from(["exename", "edit", "-m8"].iter());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn returns_an_error_when_a_year_is_passed_without_a_month() {
+        let result = Cli::new_from(["exename", "edit", "-y2020"].iter());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn returns_a_passed_value_for_edit() {
+        let cli: Cli =
+            Cli::new_from(["exename", "edit", "-h5", "-d15", "-m12", "-y2021"].iter()).unwrap();
+        let new_cli = cli.parse_commands(&cli.matches);
+        let result = new_cli.unwrap();
+        let values = unwrap_iter_with_option::<u32>(result.options);
+        assert_eq!(values, vec![5, 15, 12, 2021]);
+    }
 }
